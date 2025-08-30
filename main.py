af872faa34676e1635ee5abe39dee622681f42f0
amaarjeya <amaar.jeya@gmail.com>
2025-08-30 16:46:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ import pandas as pd
 
 from functions import *
 
-#Import data- 
-# the static files for now, then allow users to upload files themselves
+#Import demo data files- 
+# these files are static, then allow users to upload files themselves
 
 sets, setBoosterContents, setBoosterSheetCards, setBoosterContentWeights, setBoosterSheets, prices, cards, pack_prices = load_data()
 
@@ -25,7 +25,7 @@ st.title("MTG Pack EV Dashboard")
 
 # Sidebar inputs
 st.sidebar.header("Settings")
-threshold = st.sidebar.number_input("Price Threshold", value=0.0, step=0.5)
+threshold = st.sidebar.number_input("Price Threshold", value=2.0, step=0.5)
 min_date = st.sidebar.date_input("Minimum Release Date", value=pd.to_datetime("2024-06-01"))
 # Convert to pandas Timestamp for comparisons
 min_date = pd.Timestamp(min_date)
@@ -38,7 +38,7 @@ latest_packs_EV = return_EV_table(threshold=threshold, min_date=min_date)
 st.subheader("Pack Expected Value")
 
 # Pick a row height (in pixels). Adjust this until it looks good.
-row_height = 35
+row_height = 34
 
 
 # Calculate height dynamically
@@ -46,11 +46,11 @@ dynamic_height = min(len(latest_packs_EV) * row_height + 50, 1000)
 
 #DataFrame (styled)
 
-st.dataframe(return_EV_table_styled(latest_packs_EV), use_container_width=True, height= dynamic_height)
+st.dataframe(return_EV_table_styled(latest_packs_EV), width= 'stretch', height= dynamic_height)
 
 
 st.sidebar.header("Simulation Settings")
-set_input = st.sidebar.selectbox("Set", options=sets['code'].tolist(), index=0)
+set_input = st.sidebar.selectbox("Set", options=latest_packs_EV['Set Code'].unique().tolist(), index=0)
 booster_input = st.sidebar.selectbox("Booster Type", options=['play','set','draft','collector'], index=0)
 sim_size = st.sidebar.number_input("Packs per Simulation", value=100, step=100)
 samples = st.sidebar.number_input("Number of Simulations", value=100, step=100)
